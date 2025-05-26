3b6125195780e08747d7fa7209268b5a29ea86e8
ahmed <ahmedypm@gmail.com>
2025-05-27 01:46:13 +0530

    change done of task defination dded

--- main.tf
+++ main.tf
@@ -4,12 +4,47 @@ provider "aws" {
 
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "main-vpc"
+  }
 }
 
-resource "aws_subnet" "public" {
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "main"
+  }
+}
+resource "aws_subnet" "public_1" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.1.0/24"
+  availability_zone =  "us-east-1a"
   map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "public_2" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone =  "us-east-1b"
+  map_public_ip_on_launch = true
+}
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.public_1.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "b" {
+  subnet_id      = aws_subnet.public_2.id
+  route_table_id = aws_route_table.public.id
 }
 
 resource "aws_security_group" "ecs_sg" {
@@ -29,28 +64,6 @@ resource "aws_security_group" "ecs_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
-
-resource "aws_instance" "ecs_instance" {
-  ami           = "ami-12345678"
-  instance_type = "t2.micro"
-  subnet_id     = aws_subnet.public.id
-  security_groups = [aws_security_group.ecs_sg.name]
-}
-
-resource "aws_s3_bucket" "app_bucket" {
-  bucket = "my-app-bucket"
-  acl    = "private"
-}
-
-resource "aws_rds_instance" "db" {
-  engine            = "mysql"
-  instance_class    = "db.t3.micro"
-  allocated_storage = 20
-  username          = "admin"
-  password          = "password"
-  publicly_accessible = true
-}
-
 resource "aws_ecs_cluster" "my_cluster" {
   name = "my-cluster"
 }
@@ -78,3 +91,31 @@ resource "aws_ecs_task_definition" "my_task" {
   ]
   DEFINITION
 }
+
+resource "aws_instance" "ecs_instance" {
+  ami           = "ami-12345678"
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.public.id
+  security_groups = [aws_security_group.ecs_sg.name]
+}
+
+resource "aws_s3_bucket" "app_bucket" {
+  bucket = "my-app-bucket"
+  acl    = "private"
+}
+
+resource "aws_rds_instance" "db" {
+  engine            = "mysql"
+  instance_class    = "db.t3.micro"
+  allocated_storage = 20
+  username          = "admin"
+  password          = "password"
+  publicly_accessible = true
+}
+
+
+resource "aws_ecs_task_definition" "my_task" {
+  family                   = "my-task"
+  requires_compatibilities = ["EC2"]
+  network_mode             = "bridge"
+
